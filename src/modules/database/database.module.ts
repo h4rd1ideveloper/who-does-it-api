89512bb983f8021b3635bd6a329f1775352ad9cd
yan <h4rd1i@gmail.com>
2025-06-10 22:34:28 -0300

    Add entity imports and update TypeORM configuration in DatabaseModule to include specific entities

--- src/modules/database/database.module.ts
+++ src/modules/database/database.module.ts
@@ -2,13 +2,22 @@ import { Global, Module } from '@nestjs/common';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import typeorm from '../../config/typeorm';
+import { User } from './entities/user.entity';
+import { ServiceProvider } from './entities/service-provider.entity';
+import { Category } from './entities/category.entity';
+import { Review } from './entities/review.entity';
+import { ContactClick } from './entities/contact-click.entity';
+import { CategoryVisit } from './entities/category-visit.entity';
+import { ServiceProviderVisit } from './entities/service-provider-visit.entity';
+import { InvitationToken } from './entities/Invitation-token.entity';
+import { Service } from './entities/service.entity';
 
 @Global()
 @Module({
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
-      load: [typeorm]
+      load: [typeorm],
     }),
     TypeOrmModule.forRootAsync({
       imports: [ConfigModule],
@@ -16,10 +25,20 @@ import typeorm from '../../config/typeorm';
       useFactory: async (config: ConfigService) => ({
         type: 'postgres',
         url: config.get<string>('DATABASE_URL'),
-        entities: ["dist/**/*.entity{.ts,.js}"],
-        migrations: ["dist/migrations/*{.ts,.js}"],
-        synchronize: false,      // em produção deixe false e use migrações
-        migrationsRun: true,     // ou false, se preferir disparar manualmente
+        entities: [
+          Category,
+          CategoryVisit,
+          ContactClick,
+          InvitationToken,
+          Review,
+          Service,
+          ServiceProvider,
+          ServiceProviderVisit,
+          User,
+        ],
+        migrations: ['dist/migrations/*{.ts,.js}'],
+        synchronize: false, // em produção deixe false e use migrações
+        migrationsRun: true, // ou false, se preferir disparar manualmente
         logging: config.get('NODE_ENV') !== 'production',
       }),
     }),
